a6c614f74e5093597bbc1ce89df9f826ae391467
MadsOeJen <mads@wtcon.dk>
2022-03-11 14:15:11 +0100

    Caliburn.Micro setup (MVVM)

---
+++ SharkSearch/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using SharkSearch.ViewModels;
+
+namespace SharkSearch {
+    public class Bootstrapper : BootstrapperBase {
+        public Bootstrapper() {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) {
+            DisplayRootViewFor<MainViewModel>();
+        }
+
+    }
+}
