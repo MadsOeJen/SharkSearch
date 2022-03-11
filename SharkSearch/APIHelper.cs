13c13c22a231a5dca64b3cbdea4c196389d99c55
MadsOeJen <mads@wtcon.dk>
2022-03-11 15:21:30 +0100

    API Helper class creation

---
+++ SharkSearch/APIHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkSearch {
+    public class APIHelper {
+
+        public static HttpClient APIClient { get; set; }
+
+        public static void InitializeClient() {
+            APIClient = new HttpClient();
+            APIClient.DefaultRequestHeaders.Accept.Clear();
+
+        }
+    }
+}
